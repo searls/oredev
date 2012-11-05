67f84a8e874ff1731cceb3b2472c3681de582ad5
Justin Searls <searls@gmail.com>
2012-11-05 16:02:32 +0100

    start chrome without security. I'm sure the BAT doesn't work but I figured I'd try.

---
+++ bin/chrome-without-security.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --disable-web-security "file://$( cd "$( dirname "$0" )" && pwd )/../sandbox/just-in-case-specrunner.html"
